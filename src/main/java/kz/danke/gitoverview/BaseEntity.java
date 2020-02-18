bbf25e45dda570c049310233d44c470417720c70
Vladislav Dankevich <vielendanke1991@gmail.com>
2020-02-18 21:10:54 +0300

    Added new class BaseEntity

---
+++ src/main/java/kz/danke/gitoverview/BaseEntity.java
@@ -0,0 +1,25 @@
+package kz.danke.gitoverview;
+
+public class BaseEntity {
+
+    private Long id;
+
+    public boolean isNew() {
+        return this.id == null;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseEntity{" +
+                "id=" + id +
+                '}';
+    }
+}
